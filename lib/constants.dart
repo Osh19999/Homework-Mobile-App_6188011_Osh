b40ab94b85226d4dd5acdddb3976c1ea2a5e9611
Osh N <50986488+Osh19999@users.noreply.github.com>
2021-02-12 23:32:57 +0700

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,62 @@
+const BOOK_DATA = [
+  {
+    "Name":"Matida",
+    "Writer":"Roald Dahl",
+    "TotalPrice":30,
+    "image":"image-1.png"
+  },
+  {
+    "Name":"Till the last breath",
+    "Writer":"Durjoy Datta",
+    "TotalPrice":499,
+    "image":"image-2.png"
+  },
+  {
+    "Name":"Noir",
+    "Writer":"A crimson shadow novel",
+    "TotalPrice":149,
+    "image":"image-3.png"
+  },
+  {
+    "Name":"Istanbul Passage",
+    "Writer":"Joseph Kanon",
+    "TotalPrice":299,
+    "image":"image-4.png"
+  },
+  {
+    "Name":"Dark Gift",
+    "Writer":"Kim Richardson",
+    "TotalPrice":949,
+    "image":"image-5.png"
+  },
+  {
+    "Name":"The Gifted",
+    "Writer":"Asia Books",
+    "TotalPrice":449,
+    "image":"image-6.png"
+  },
+  {
+    "Name":"Wings of Fire",
+    "Writer":"The graphic novel",
+    "TotalPrice":179,
+    "image":"image-7.png"
+  },
+  {
+    "Name":"TroubledBlood",
+    "Writer":"Robert Galbraith",
+    "TotalPrice":299,
+    "image":"image-8.png"
+  },
+  {
+    "Name":"The novel happiness",
+    "Writer":"Alexander McCall Smith",
+    "TotalPrice":699,
+    "image":"image-9.png"
+  },
+  {
+    "Name":"DUNE",
+    "Writer":"BOOM",
+    "TotalPrice":699,
+    "image":"image-10.png"
+  }
+];
